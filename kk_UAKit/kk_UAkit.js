12e7921b151557e3d3e915a0c3f40eb78e57228d
liuyuanquan <liuyuanquan@kankan.com>
2016-09-14 11:19:14 +0800

    update kk_UAKit.js

--- kk_UAKit/kk_UAkit.js
+++ kk_UAKit/kk_UAkit.js
@@ -10,36 +10,65 @@
 		global[exportsName] = factory(global);
 	}
 })(typeof window !== "undefined" ? window : this, function(window){
+	var ua = window.navigator.userAgent;
 	return {
-		isPC: function(ua){
-			return !this.isMobile(ua);
+		isPC: function(){
+			return !this.isMobile() && !this.isIPad() && !this.isAndroidPad();
 		},
-		isIE: function(ua){
-
+		//IE11没有“MSIE” 没有包括Edge
+		isIE: function(){
+			return /(msie\s|trident.*rv:)(\d+)/i.test(ua);
 		},
-		isQQ: function(ua){
+		//IE5与IE7不能区分 不能区分Edge
+		getIEVersion: function () {
+		    var m = ua.match(/(MSIE\s|trident.*rv:)(\d+)/i);
+		    return (m && m.length > 2) ? +m[2] : -1;
+		},
+		isEdge: function(){
+			return /Edge/ig.test(ua);
+		},
+		isQQ: function(){
 			return /QQBrowser/ig.test(ua);
 		},
-		isMobile: function(ua){
+		isUC: function () {
+		    return /UCBrowser/ig.test(ua);
+		},
+		isFF: function() {
+			return /Firefox/ig.test(ua);
+		},
+		isChrome: function(){
+			return /Chrome/ig.test(ua);
+		},
+		isSafari: function(){
+			return !this.isChrome() && /Safari/ig.test(ua);
+		},
+		isMobile: function(){
 			return /Mobile/ig.test(ua);
 		},
-		isIOS: function(ua){
-			return /OS/ig.test(ua);
+		isIOS: function(){
+			return /iPhone|iPad|iPod/ig.test(ua);
 		},
-		isIPhone: function(ua){
+		isIPhone: function(){
 			return /iPhone/ig.test(ua);
 		},
-		isIPad: function(ua){
+		isIPad: function(){
 			return /iPad/ig.test(ua);
 		},
-		isIPod: function(ua){
+		isIPod: function(){
 			return /iPod/ig.test(ua);
 		},
-		isAndroid: function(ua){
+		isAndroid: function(){
 			return /Android/ig.test(ua);
 		},
-		isWeiXin: function(ua){
-
+		isAndroidPad: function () {
+		    return this.isAndroid() && !this.isMobile();
+		},
+		isWeiXin: function(){
+			return /MicroMessenger/ig.test(ua);
+		},
+		//明星空间专用
+		isVstarApp: function () {
+		    return /vstar/ig.test(ua);
 		}
 	};
 }, "kk_UAKit");
