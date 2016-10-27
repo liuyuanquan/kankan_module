0075ee2380cd501ae85bfe4ef34c94703e7da7b5
liuyuanquan <liuyuanquan@kankan.com>
2016-10-27 15:34:03 +0800

    update device.js

--- device/src/device_es2015.js
+++ device/src/device_es2015.js
@@ -15,17 +15,26 @@ const version2number = version => {
 	return counter;
 }
 
+const userAgentStrs = {
+	Edge: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586'
+	IE11: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; SLCC2; rv:11.0) like Gecko',
+	IE10: 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 10.0; Win64; x64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; SLCC2)',
+	IE09: 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 10.0; Win64; x64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; SLCC2)',
+	IE08: 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 10.0; Win64; x64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; SLCC2)',
+	IE07: 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 10.0; Win64; x64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; SLCC2)',
+	IE05: 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 10.0; Win64; x64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; SLCC2)'
+}
+
 const device_es2015 = {
-	//IE11没有“MSIE”，没有包括Edge
 	isIE: () => {
-		return /(MSIE\s|trident.*rv:)(\d+)/i.test(ua);
+		return /(MSIE\s|Trident.*rv:)(\d+)/i.test(ua);
 	},
 	isEdge: () => {
 		return /Edge/ig.test(ua);
 	},
 	//IE5与IE7不能区分，不能区分Edge
 	getIEVersion: () => {
-	    let m = ua.match(/(MSIE\s|trident.*rv:)(\d+)/i);
+	    let m = ua.match(/(MSIE\s|Trident.*rv:)(\d+)/i);
 	    return (m && m.length > 2) ? +m[2] : -1;
 	},
 	isQQ: () => {
